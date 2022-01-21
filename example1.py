8c20e3fd2de15addcff7cd76753b32b8b146f069
Vasiliy Kirnos <vasikir@xenzu.com>
2022-01-21 21:19:56 +0300

    change name of screen

--- example1.py
+++ example1.py
@@ -10,5 +10,5 @@ driver = webdriver.Chrome("/Users/vasiliykirnos/skillfactory/python_selenium_sf/
 driver.get("http://130.193.37.179/app/pets")
 (driver.find_elements(By.XPATH, "//*[@id=\"image\"]/img"))[0].click()
 sleep(3)
-driver.save_screenshot('pet_home.png')
+driver.save_screenshot('pet_home12345.png')
 driver.quit()
